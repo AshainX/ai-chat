47b453a8a3e7c3fc05a015d6e1eb168ec11687f4
Ashutosh Swain <79051730+AshainX@users.noreply.github.com>
2025-01-26 23:50:37 +0530

    npx convex dev

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} 
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
